264d404d1b328edf4b93c499b66af77115290cd2
Fadli <fadlimuharram@hotmail.com>
2017-04-05 11:54:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-//ALLOWING CORS
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 /**
  * CodeIgniter
  *
